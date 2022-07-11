d25f6213c058cc7e6e5cd57fe4124ec6bf1b6c73
igor.milovanovic13 <igor.milovanovic13@gmail.com>
2022-07-12 00:50:08 +0200

    First tests, settings initial Projects routes, Model, Controller and Factory

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function store()
+    {
+        // validate
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+        // persist
+        Project::create($attributes);
+        // redirect
+        return redirect('/projects');
+    }
+}
